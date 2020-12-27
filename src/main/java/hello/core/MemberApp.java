880414f9b50b93b01307956f2abf497c29b84168
harper-joo-lee <lamantciel@naver.com>
2020-12-27 12:36:58 +0900

    update - 빈 테스트

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,22 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+
         //MemberService memberService = new MemberServiceImpl();
+
+        // 스프링 컨테이너
+        // Appconfig에 있는 환경설정 정보를 가지고 스프링이 Bean 어노테이션이 붙은 메소드들을 Bean 에 등록한다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
